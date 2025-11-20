1735458e817b0ccd45221add24a7cbd4de02e05c
Nathalie Duong <dnthlie@gmail.com>
2025-11-20 00:47:01 -0800

    Changed start scene to test out fxml file with alien images/text

--- src/main/java/org/example/invitro/GameUI.java
+++ src/main/java/org/example/invitro/GameUI.java
@@ -1,23 +1,25 @@
 package org.example.invitro;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class GameUI extends Application {
 
     public static void main(String[] args) { launch(args); }
 
+    //Nat - TESTING MainWindow.fxml >> Will change to a different screen at later time for start window
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("JavaFX is working!");
-        StackPane root = new StackPane(label);
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
 
-        Scene scene = new Scene(root, 400, 200);
+        Scene scene = new Scene(loader.load(), 690, 690);
 
-        primaryStage.setTitle("JavaFX Test Window");
+        primaryStage.setTitle("Main Window");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
