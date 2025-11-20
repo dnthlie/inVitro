821d7afb5acac2ecea84b3f4046c9dfece56f219
Nathalie Duong <dnthlie@gmail.com>
2025-11-19 23:36:37 -0800

    Patch-3

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.example.invitro {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.invitro to javafx.fxml;
+    exports org.example.invitro;
+}
---
+++ src/main/java/org/example/invitro/GameUI.java
@@ -0,0 +1,26 @@
+package org.example.invitro;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GameUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("JavaFX is working!");
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        primaryStage.setTitle("JavaFX Test Window");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
